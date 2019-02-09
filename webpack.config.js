98c52c509774ad8759d1f6321baedf9755c3cfe4
hsimao <e087754958@gmail.com>
2019-02-09 14:59:27 +0800

    add: image optimize

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,6 @@
 // 避免不同作業系統路徑不一致
 const path = require("path");
 
-// 獨立拆分css檔案插件
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const extractCSS = new ExtractTextPlugin("css/[name].css");
-
 module.exports = {
   mode: process.env.NODE_ENV,
   context: path.resolve(__dirname, "./src"),
@@ -24,6 +20,7 @@ module.exports = {
       path.resolve("src"),
       path.resolve("src/js"),
       path.resolve("src/sass"),
+      path.resolve("src/images"),
       path.resolve("node_modules")
     ],
     extensions: [".js"] // 引入時副檔名.js可省略不寫
@@ -63,24 +60,54 @@ module.exports = {
       // css loader
       {
         test: /\.css$/,
-        use: extractCSS.extract(["css-loader", "postcss-loader"])
+        use: ["style-loader", "css-loader", "postcss-loader"]
       },
-      // sass、scss loader - 將css寫入js版本
-      // {
-      //   test: /\.(sass|scss)$/,
-      //   use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
-      // },
-      // sass、scss loader - 將css拆出獨立檔案版本
       {
         test: /\.(sass|scss)$/,
-        use: extractCSS.extract(["css-loader", "postcss-loader", "sass-loader"])
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
       },
       // babel 編譯
       {
         test: /\.js$/,
         use: "babel-loader"
+      },
+      /*
+        圖片優化1 壓縮圖片 image-webpack-loader
+        圖片優化2 url-loader
+        1.) 將小圖檔轉譯為64base格式，減少http請求，降低server負擔
+        2.) 圖片檔名加入hash更新快取
+      */
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192, // 檔案大小超過8k的圖片不轉譯，只更新以下檔名，加入hash更新快取
+              name: "[path][name].[ext]?[hash:8]"
+            }
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: "65-90",
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              }
+            }
+          }
+        ]
       }
     ]
-  },
-  plugins: [extractCSS]
+  }
 };
